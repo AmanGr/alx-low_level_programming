b87a76f593c323c4ca7ab97d4b85fa62cda042ff
AmanGr <alphagr110@gmail.com>
2021-08-26 10:08:04 -0700

    code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+  
+/* The printf function reads and prints characters on the screen */
+
+#include <stdio.h>
+
+/**
+ * main - prints "with proper grammer, but the outcome is a piece of art,
+ *
+ * Return: again 0 for void
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
