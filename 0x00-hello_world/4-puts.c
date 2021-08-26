5cd5236e336e1bf41568b5583dc201f291e71291
AmanGr <alphagr110@gmail.com>
2021-08-26 10:06:06 -0700

    code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+/* the puts reads and prints to <stdout> aka screen */
+
+#include <stdio.h>
+
+/**
+* main - prints "Programming is like building a multilingual puzzle"
+*
+* Return: always 0 when main is set to void for int.
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
