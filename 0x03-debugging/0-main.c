f39010c36e6aa3899ebd653347d5939e34130e2b
AmanGr <alphagr110@gmail.com>
2021-09-01 11:49:15 -0700

    code

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
+ *main - Entry point
+ *
+ *Description: trying function that prints if integer is positive or negative.
+ *positive, negative, or zero.
+ *Return: Returns 0.
+ */
 int main(void)
 {
-	int i;
+int i;
 
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
+i = 0;
+positive_or_negative(i);
+return (0);
 }
