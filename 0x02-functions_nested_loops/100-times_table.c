7b20d30db7e50ab7d1dc903d67d88fc2b27ff65d
AmanGr <alphagr110@gmail.com>
2021-09-01 10:45:26 -0700

    code

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,55 +1,52 @@
-#include "main.h"
+#include "holberton.h"
+#include <unistd.h>
 
 /**
- * print_times_table - prints times table for numbers from 0-14
- * @n: An input integer value
- * Return: Nothing
+ * print_times_table- prints inputed times table
+ * @n: holds the number for the times table
+ *
  */
+
 void print_times_table(int n)
 {
-	int i, j;
+	int i;
+	int j;
+	int k;
+	int d;
+	int a;
 
-	if (n > 0 && n < 15)
+	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i <= n; i++)
+		for (j = 0; j <= n; j += 1)
 		{
 			_putchar('0');
-			for (j = 1; j <= n; j++)
-				putformat(i * j);
+			for (i = 1; i <= n; i += 1)
+			{
+				d = i * j;
+				_putchar(',');
+				_putchar(' ');
+				a = d % 10;
+				k = d / 10;
+				if (d >= 100) /* triple digits */
+				{
+					_putchar(k / 10 + '0');
+					_putchar(d / 10 % 10 + '0');
+					_putchar(a + '0');
+				}
+				else if (d >= 10 && d < 100) /* double digit */
+				{
+					_putchar(' ');
+					_putchar(k + '0');
+					_putchar(a + '0');
+				}
+				else /*single digit */
+				{
+					 _putchar(' ');
+					 _putchar(' ');
+					 _putchar(d % 10 + '0');
+				}
+			}
 			_putchar('\n');
 		}
-	}
-}
-
-/**
- * putformat - formatted characters to output
- * @n: number to format
- * Return: nothing
- */
-void putformat(int n)
-{
-	if (n <= 9)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n + '0');
-	}
-	else if (n > 9 && n <= 99)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n / 100 + '0');
-		_putchar(n / 10 % 10 + '0');
-		_putchar(n % 10 + '0');
 	}
 }
