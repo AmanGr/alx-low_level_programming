2df47db71bef67e04ecce3d4e4ad63a52cba4014
AmanGr <alphagr110@gmail.com>
2021-09-06 13:33:47 -0700

    C_code

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,28 +1,27 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_array - Entry point
- * Description - A function that prints n elements of
- * an array of integers, followed by a new line
- * where n is the number of elements of the array to be printed
- * Numbers must be separated by comma, followed by a space
- * The numbers should be displayed in the same order as they are stored in the array
- * You are allowed to use printf
- * *@a: the function accepts an input saved into a
- * *@n: the function accepts an input saved into n
- * Return: Always 0 (Success)
+ * puts_half - prints half of a string.
+ * @str: input string.
+ * Return: no return.
  */
-void print_array(int *a, int n)
+void puts_half(char *str)
 {
-int count = 0;
+	int count = 0, i;
 
-while (count < n)
-{
-printf("%d", *(a + count));
-if (count != (n - 1))
-printf(", ");
-count++;
-}
-printf("\n");
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+			break;
+		count++;
+	}
+
+	if (count % 2 == 1)
+		i = count / 2;
+	else
+		i = (count - 1) / 2;
+
+	for (i++; i < count; i++)
+		_putchar(str[i]);
+	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,28 +1,21 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_array - Entry point
- * Description - A function that prints n elements of
- * an array of integers, followed by a new line
- * where n is the number of elements of the array to be printed
- * Numbers must be separated by comma, followed by a space
- * The numbers should be displayed in the same order as they are stored in the array
- * You are allowed to use printf
- * *@a: the function accepts an input saved into a
- * *@n: the function accepts an input saved into n
- * Return: Always 0 (Success)
+ * print_array - prints n elements of an array of integers.
+ * @a: input array.
+ * @n: input n elements
+ * Return: no return.
  */
 void print_array(int *a, int n)
 {
-int count = 0;
+	int i = 0;
 
-while (count < n)
-{
-printf("%d", *(a + count));
-if (count != (n - 1))
-printf(", ");
-count++;
-}
-printf("\n");
+	for (; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
 }
