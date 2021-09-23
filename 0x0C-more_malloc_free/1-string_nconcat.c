8e6b3300e7ccb5a1fe4d208fb5b6dca8abb8aab8
AmanGr <alphagr110@gmail.com>
2021-09-23 14:37:27 -0700

    code

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,52 @@
 #include "main.h"
-#include <stdlib.h>
-
 /**
-  * string_nconcat - ...
-  * @s1: ...
-  * @s2: ...
-  * @n: ...
-  *
-  * Return: ...
-  */
+ * string_nconcat - function that concatenates two strings.
+ * @s1: firts tring to concatenate
+ * @s2: second string to concatenate
+ * @n: is the limit to return
+ * Return: the pointer 'ptr'
+ */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i = 0, j = 0, k = 0, l = 0;
-char *str;
+	unsigned int i = 0, j = 0, k, l;
+	char *ptr;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-while (s1[i])
-i++;
-while (s2[k])
-k++;
-if (n >= k)
-l = i + k;
-else
-l = i + n;
-str = malloc(sizeof(char) * l + 1);
-if (str == NULL)
-return (NULL);
-k = 0;
-while (j < l)
-{
-if (j <= i)
-str[j] = s1[j];
+	while (*(s1 + i) != '\0') /* recorrido de s1 */
+	{
+		i++;
+	}
+	while (*(s2 + j) != '\0')/* recorrido de s2 */
+	{
+		j++;
+	}
+	if (n < j)
+	{
+		j = n;
+	}
 
-if (j >= i)
-{
-str[j] = s2[k];
-k++;
-}
-j++;
-}
-str[j] = '\0';
-return (str);
+	ptr = malloc(sizeof(char) * (i + j + 1)); /* We obtain the malloc. */
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (k = 0; k < i ; k++)
+	{
+		*(ptr + k) = *(s1 + k);
+	}
+
+	for (l = 0 ; l < j ; l++)
+	{
+		*(ptr + (k + l)) = *(s2 + l);
+	}
+
+	*(ptr + (k + l)) = '\0';
+	return (ptr);
 }
