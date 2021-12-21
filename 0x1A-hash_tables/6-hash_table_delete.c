515041fafd0512a9fd5c9b8951db33271209b171
AmanGr <alphagr110@gmail.com>
2021-12-21 12:22:25 -0800

    code

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: hash_node_t list to be freed
+ *Return: Void
+ */
+void free_list(hash_node_t *head)
+{
+	hash_node_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->key);
+		free(head->value);
+		free(head);
+		head = temp;
+	}
+}
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table to be deleted
+ *Return: Void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+		free_list(ht->array[i]);
+	free(ht->array);
+	free(ht);
+}
