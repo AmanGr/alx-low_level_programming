cf37c63f378ab4df97f624d7f83a483f81579b92
AmanGr <alphagr110@gmail.com>
2022-02-17 12:05:15 -0800

    code

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,36 +1,20 @@
 #!/usr/bin/python3
-"""Contains function that returns the perimeter of the
-island described in grid
-"""
-
-
-def determine_soroundings(array, y, x):
-    """Determines whether soroundings has water or not
-    also if it's next or previous is at the ends
-    Args:
-        array (list): grid
-        y (int): current list index in `array`
-        x (int): current index in the list in `array`
-    """
-    mask = 1
-    top = array[y - 1][x] ^ mask if y > 0 else 1
-    bottom = array[y + 1][x] ^ mask if y < (len(array) - 1) else 1
-    left = array[y][x - 1] ^ mask if x > 0 else 1
-    right = array[y][x + 1] ^ mask if x < (len(array[y]) - 1) else 1
-    positions = top + bottom + right + left
-    return positions
+"""returns the perimeter of the island described in grid(int list of list)"""
 
 
 def island_perimeter(grid):
-    """Gets the perimeter determined by sorounding.
-    all sides that do not have water we increase perimeter.
-    Args:
-        grid (list): list of lists
-    """
-    land = 1
-    perimeter = 0
-    for i in range(len(grid)):
-        for j, x in enumerate(grid[i]):
-            if x == land:
-                perimeter += determine_soroundings(grid, i, j)
-    return perimeter
+    """Return the island perimeter"""
+    wid = len(grid[0])
+    hei = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(hei):
+        for j in range(wid):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (1 > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
